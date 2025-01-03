01a7da035223dd19870428e3533bb4b7b1473d1d
Son Tran Thanh <286.trants@gmail.com>
2025-01-04 01:11:50 +0700

    6

--- pkg/encoding/binary/endian_le.go
+++ pkg/encoding/binary/endian_le.go
@@ -10,127 +10,104 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
+	"math"
+
 	"github.com/focela/loom/internal/core"
 	"github.com/focela/loom/pkg/errors"
-	"math"
 )
 
-// LeEncode encodes one or multiple `values` into bytes using LittleEndian.
-// It uses type asserting checking the type of each value of `values` and internally
-// calls corresponding converting function do the bytes converting.
-//
-// It supports common variable type asserting, and finally it uses fmt.Sprintf converting
-// value to string and then to bytes.
+// LeEncode encodes multiple values into bytes using LittleEndian.
 func LeEncode(values ...interface{}) []byte {
 	buf := new(bytes.Buffer)
-	for i := 0; i < len(values); i++ {
-		if values[i] == nil {
+	for _, value := range values {
+		if value == nil {
 			return buf.Bytes()
 		}
-		switch value := values[i].(type) {
+		switch v := value.(type) {
 		case int:
-			buf.Write(LeEncodeInt(value))
+			buf.Write(LeEncodeInt(v))
 		case int8:
-			buf.Write(LeEncodeInt8(value))
+			buf.Write(LeEncodeInt8(v))
 		case int16:
-			buf.Write(LeEncodeInt16(value))
+			buf.Write(LeEncodeInt16(v))
 		case int32:
-			buf.Write(LeEncodeInt32(value))
+			buf.Write(LeEncodeInt32(v))
 		case int64:
-			buf.Write(LeEncodeInt64(value))
+			buf.Write(LeEncodeInt64(v))
 		case uint:
-			buf.Write(LeEncodeUint(value))
+			buf.Write(LeEncodeUint(v))
 		case uint8:
-			buf.Write(LeEncodeUint8(value))
+			buf.Write(LeEncodeUint8(v))
 		case uint16:
-			buf.Write(LeEncodeUint16(value))
+			buf.Write(LeEncodeUint16(v))
 		case uint32:
-			buf.Write(LeEncodeUint32(value))
+			buf.Write(LeEncodeUint32(v))
 		case uint64:
-			buf.Write(LeEncodeUint64(value))
+			buf.Write(LeEncodeUint64(v))
 		case bool:
-			buf.Write(LeEncodeBool(value))
+			buf.Write(LeEncodeBool(v))
 		case string:
-			buf.Write(LeEncodeString(value))
+			buf.Write(LeEncodeString(v))
 		case []byte:
-			buf.Write(value)
+			buf.Write(v)
 		case float32:
-			buf.Write(LeEncodeFloat32(value))
+			buf.Write(LeEncodeFloat32(v))
 		case float64:
-			buf.Write(LeEncodeFloat64(value))
-
+			buf.Write(LeEncodeFloat64(v))
 		default:
-			if err := binary.Write(buf, binary.LittleEndian, value); err != nil {
+			if err := binary.Write(buf, binary.LittleEndian, v); err != nil {
 				core.Errorf(context.TODO(), `%+v`, err)
-				buf.Write(LeEncodeString(fmt.Sprintf("%v", value)))
+				buf.Write(LeEncodeString(fmt.Sprintf("%v", v)))
 			}
 		}
 	}
 	return buf.Bytes()
 }
 
+// LeEncodeByLength encodes values into bytes and adjusts the length.
 func LeEncodeByLength(length int, values ...interface{}) []byte {
 	b := LeEncode(values...)
 	if len(b) < length {
 		b = append(b, make([]byte, length-len(b))...)
 	} else if len(b) > length {
-		b = b[0:length]
+		b = b[:length]
 	}
 	return b
 }
 
-func LeDecode(b []byte, values ...interface{}) error {
-	var (
-		err error
-		buf = bytes.NewBuffer(b)
-	)
-	for i := 0; i < len(values); i++ {
-		if err = binary.Read(buf, binary.LittleEndian, values[i]); err != nil {
-			err = errors.Wrap(err, `binary.Read failed`)
-			return err
-		}
-	}
-	return nil
-}
-
+// LeEncodeString encodes a string into bytes.
 func LeEncodeString(s string) []byte {
 	return []byte(s)
 }
 
-func LeDecodeToString(b []byte) string {
-	return string(b)
-}
-
+// LeEncodeBool encodes a boolean into a byte.
 func LeEncodeBool(b bool) []byte {
 	if b {
 		return []byte{1}
-	} else {
-		return []byte{0}
 	}
+	return []byte{0}
 }
 
 func LeEncodeInt(i int) []byte {
 	if i <= math.MaxInt8 {
-		return EncodeInt8(int8(i))
+		return LeEncodeInt8(int8(i))
 	} else if i <= math.MaxInt16 {
-		return EncodeInt16(int16(i))
+		return LeEncodeInt16(int16(i))
 	} else if i <= math.MaxInt32 {
-		return EncodeInt32(int32(i))
-	} else {
-		return EncodeInt64(int64(i))
+		return LeEncodeInt32(int32(i))
 	}
+	return LeEncodeInt64(int64(i))
 }
 
 func LeEncodeUint(i uint) []byte {
 	if i <= math.MaxUint8 {
-		return EncodeUint8(uint8(i))
+		return LeEncodeUint8(uint8(i))
 	} else if i <= math.MaxUint16 {
-		return EncodeUint16(uint16(i))
+		return LeEncodeUint16(uint16(i))
 	} else if i <= math.MaxUint32 {
-		return EncodeUint32(uint32(i))
-	} else {
-		return EncodeUint64(uint64(i))
+		return LeEncodeUint32(uint32(i))
 	}
+	return LeEncodeUint64(uint64(i))
 }
 
 func LeEncodeInt8(i int8) []byte {
@@ -178,64 +155,36 @@ func LeEncodeUint64(i uint64) []byte {
 }
 
 func LeEncodeFloat32(f float32) []byte {
-	bits := math.Float32bits(f)
 	b := make([]byte, 4)
-	binary.LittleEndian.PutUint32(b, bits)
+	binary.LittleEndian.PutUint32(b, math.Float32bits(f))
 	return b
 }
 
 func LeEncodeFloat64(f float64) []byte {
-	bits := math.Float64bits(f)
 	b := make([]byte, 8)
-	binary.LittleEndian.PutUint64(b, bits)
+	binary.LittleEndian.PutUint64(b, math.Float64bits(f))
 	return b
 }
 
-func LeDecodeToInt(b []byte) int {
-	if len(b) < 2 {
-		return int(LeDecodeToUint8(b))
-	} else if len(b) < 3 {
-		return int(LeDecodeToUint16(b))
-	} else if len(b) < 5 {
-		return int(LeDecodeToUint32(b))
-	} else {
-		return int(LeDecodeToUint64(b))
+func LeDecode(b []byte, values ...interface{}) error {
+	buf := bytes.NewBuffer(b)
+	for _, value := range values {
+		if err := binary.Read(buf, binary.LittleEndian, value); err != nil {
+			return errors.Wrap(err, `binary.Read failed`)
+		}
 	}
-}
-
-func LeDecodeToUint(b []byte) uint {
-	if len(b) < 2 {
-		return uint(LeDecodeToUint8(b))
-	} else if len(b) < 3 {
-		return uint(LeDecodeToUint16(b))
-	} else if len(b) < 5 {
-		return uint(LeDecodeToUint32(b))
-	} else {
-		return uint(LeDecodeToUint64(b))
-	}
+	return nil
 }
 
 func LeDecodeToBool(b []byte) bool {
-	if len(b) == 0 {
-		return false
-	}
-	if bytes.Equal(b, make([]byte, len(b))) {
-		return false
-	}
-	return true
+	return len(b) > 0 && b[0] != 0
 }
 
 func LeDecodeToInt8(b []byte) int8 {
-	if len(b) == 0 {
-		panic(`empty slice given`)
-	}
 	return int8(b[0])
 }
 
 func LeDecodeToUint8(b []byte) uint8 {
-	if len(b) == 0 {
-		panic(`empty slice given`)
-	}
 	return b[0]
 }
 
@@ -247,34 +196,6 @@ func LeDecodeToUint16(b []byte) uint16 {
 	return binary.LittleEndian.Uint16(LeFillUpSize(b, 2))
 }
 
-func LeDecodeToInt32(b []byte) int32 {
-	return int32(binary.LittleEndian.Uint32(LeFillUpSize(b, 4)))
-}
-
-func LeDecodeToUint32(b []byte) uint32 {
-	return binary.LittleEndian.Uint32(LeFillUpSize(b, 4))
-}
-
-func LeDecodeToInt64(b []byte) int64 {
-	return int64(binary.LittleEndian.Uint64(LeFillUpSize(b, 8)))
-}
-
-func LeDecodeToUint64(b []byte) uint64 {
-	return binary.LittleEndian.Uint64(LeFillUpSize(b, 8))
-}
-
-func LeDecodeToFloat32(b []byte) float32 {
-	return math.Float32frombits(binary.LittleEndian.Uint32(LeFillUpSize(b, 4)))
-}
-
-func LeDecodeToFloat64(b []byte) float64 {
-	return math.Float64frombits(binary.LittleEndian.Uint64(LeFillUpSize(b, 8)))
-}
-
-// LeFillUpSize fills up the bytes `b` to given length `l` using LittleEndian.
-//
-// Note that it creates a new bytes slice by copying the original one to avoid changing
-// the original parameter bytes.
 func LeFillUpSize(b []byte, l int) []byte {
 	if len(b) >= l {
 		return b[:l]
